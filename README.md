e0c08d814f50a2efdd81bd5bffc06be825ecc2ae
Varun Patil <varunpatil@ucla.edu>
2022-10-11 11:39:41 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Demo Server
+
+For developers only.
+
+This app allows you to freeze a Nextcloud server with auto-login for demo. Just deploy a Nextcloud instance
+with this app enabled and you can use it as a showcase for your application.
---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\DemoServer\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\IRequest;
+use OCP\IUserSession;
+use OCA\DemoServer\Listener;
+
+class Application extends App implements IBootstrap
+{
+    private $appName = 'demoserver';
+
+    public function __construct()
+    {
+        parent::__construct($this->appName);
+    }
+
+    public function register($context): void
+    {
+        $context->registerEventListener(\OCP\Files\Events\Node\BeforeNodeCreatedEvent::class, Listener::class);
+    }
+
+    public function boot(IBootContext $context): void
+    {
+        $uid = 'demo'; $password = '56183891677321143025';
+
+        $container = $context->getAppContainer();
+        $userSession = $container->query(IUserSession::class);
+        $request = $container->query(IRequest::class);
+
+        // Check for nodemo query param
+        if ($request->getParam('nodemo') === '1') {
+            return;
+        }
+
+        // Check if the user is logged in
+        if (!$userSession->isLoggedIn()) {
+            $userSession->createSessionToken($request, $uid, $uid, $password);
+            $userSession->createRememberMeToken($userSession->getUser());
+        }
+    }
+}
